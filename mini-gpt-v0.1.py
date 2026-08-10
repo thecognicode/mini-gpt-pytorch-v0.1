f20697ffbb16a527b34fda6151c1425bde7a32fd
Thecognicode <theabisheksam@gmail.com>
2026-08-10 12:20:02 +0530

    Embeddings Added

--- mini-gpt-v0.1.py
+++ mini-gpt-v0.1.py
@@ -5,3 +5,7 @@ import torch.nn.functional as F
 class MiniGPT(nn.module):
     def __init__(self, vocab_size, embed_dim, num_heads, num_layers, max_seq_len):
         super().__init__()
+
+        # 1. Embeddings
+        self.token_embedding = nn.Embedding(vocab_size, embed_dim)
+        self.position_embedding = nn.Embedding(max_seq_len, embed_dim)
