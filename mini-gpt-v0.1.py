21529737f79b318bb04186df6f99dde763293628
Thecognicode <theabisheksam@gmail.com>
2026-08-10 12:23:01 +0530

    Transformer Decoder added

--- mini-gpt-v0.1.py
+++ mini-gpt-v0.1.py
@@ -9,3 +9,13 @@ class MiniGPT(nn.module):
         # 1. Embeddings
         self.token_embedding = nn.Embedding(vocab_size, embed_dim)
         self.position_embedding = nn.Embedding(max_seq_len, embed_dim)
+
+        # Stacking Transformer Decoder
+        self.blocks = nn.ModuleList([
+            nn.TransformerEncoderLayer(
+                d_model = embed_dim,
+                nheads = num_heads,
+                dim_feedforward=4 * embed_dim,
+                batch_first = True
+            ) for _ in range(num_layers)
+        ])
